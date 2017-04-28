740a5566ce434807edf7e8276b68ad95bcc408c6
Cristiano Soleti <cristiano.soleti@edu.unito.it>
2017-04-28 11:14:03 +0200

    Bugfix

---
+++ src/SYSTEM_CONSTANTS.java
@@ -0,0 +1,12 @@
+
+public class SYSTEM_CONSTANTS {
+
+	public static final String DELETE_ACTION = "DELETE";
+	public static final String FORWARD_ACTION = "FORWARD";
+	public static final String CREATE_ACTION = "CREATE";
+	public static final String SEND_ACTION = "SEND";
+	public static final String LOAD_ACTION = "LOAD";
+
+
+
+}
